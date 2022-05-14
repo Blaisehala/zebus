d623cd16d080c872ef58a1a8ab8825b244ababdb
Blaise Hala <blaisehala@gmail.com>
2022-05-14 16:17:20 +0300

    ft:looping through posts

--- zebus.py
+++ zebus.py
@@ -2,17 +2,34 @@ from flask import Flask,render_template
 app = Flask(__name__)
 
 
+posts = [
+  
+  {
+    'author': 'Blaise Hala',
+    'title': 'Blog Post',
+    'content': 'First post content ',
+    'dete_posted':'April 2014'
+  },
+
+ {
+    'author': 'Blaise Hala',
+    'title': 'Blog Post',
+    'content': 'First post content',
+    'date_posted':'April 2014'
+ }
+]
+
 
 @app.route('/')
 @app.route ('/home')
 def home():
-  return render_template('home.html')
+  return render_template ('home.html', posts=posts)
 
 
 
 @app.route('/about')
 def about():
-  return 'About US'
+  return render_template('about.html')
 
 
   if __name__ == '__main__':  
