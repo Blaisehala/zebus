2cf94e44d17e6cd7a4f5c319bf753d657a5d5369
Blaise Hala <blaisehala@gmail.com>
2022-05-18 07:31:03 +0300

    feat: adds the delete post route

--- zebus/routes.py
+++ zebus/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template,url_for,flash,redirect, request
+from flask import render_template,url_for,flash,redirect, request,abort
 from flask_login import login_user,current_user, logout_user, login_required
 from zebus import app,db,bcrypt
 from zebus.forms import  RegistrationForm, LoginForm, PostForm
@@ -90,5 +90,32 @@ def new_post():
     flash('Your post has been created successfully!', 'info')
     return redirect (url_for("home"))
 
-  return render_template('create_post.html', title= 'New Post', form=form)
+  return render_template('create_post.html', title= 'New Post', form=form, legend='New Post')
+
+
+@app.route('/post/<int:post_id>')
+def post (post_id):
+  post = Post.query.get_or_404(post_id)
+  return render_template('post.html', title=post.title, post=post)
+
+
+@app.route('/post/<int:post_id>/update', methods=['GET','POST'])
+@login_required
+def update_post(post_id):
+   post = Post.query.get_or_404(post_id)
+   if post.author!= current_user:
+     abort(403)
+   form = PostForm()
+   if form.validate_on_submit():
+     post.title = form.title.data
+     post.content = form.content.data
+     db.session.commit()
+     flash ('Your post has been updated successfully')
+     return redirect(url_for('post', post_id=post.id))
+   
+   elif  request.method == 'GET':
+     form.title.data = post.title
+     form.content.data = post.content
+   return render_template('create_post.html', title='Update post', form=form, legend='Update Post')
+
 
